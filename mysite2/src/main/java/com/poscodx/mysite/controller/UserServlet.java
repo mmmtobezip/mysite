8dc482a0b63c2c4da0d5f4826c784a705fca8374
mmmtobezip <mmmszip@gmail.com>
2024-05-27 14:56:30 +0900

    refactor: factory method를 이용한 User 기능 분리

--- mysite2/src/main/java/com/poscodx/mysite/controller/UserServlet.java
+++ mysite2/src/main/java/com/poscodx/mysite/controller/UserServlet.java
@@ -1,55 +1,71 @@
 package com.poscodx.mysite.controller;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import com.poscodx.mysite.dao.UserDao;
-import com.poscodx.mysite.vo.UserVo;
+import java.util.Map;
+import com.poscodx.mysite.controller.action.main.MainAction;
+import com.poscodx.mysite.controller.action.user.JoinAction;
+import com.poscodx.mysite.controller.action.user.JoinFormAction;
+import com.poscodx.mysite.controller.action.user.JoinSuccess;
+import com.poscodx.mysite.controller.action.user.LoginAction;
+import com.poscodx.mysite.controller.action.user.LoginFormAction;
 
-public class UserServlet extends HttpServlet {
+public class UserServlet extends ActionServlet {
   private static final long serialVersionUID = 1L;
+  
+  private Map<String, Action> mapAction = Map.of(
+    "joinform", new JoinFormAction(),
+    "join", new JoinAction(),
+    "joinsuccess", new JoinSuccess(),
+    "loginform", new LoginFormAction(),
+    "login", new LoginAction()
+  );
+      
 
-  protected void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
-    request.setCharacterEncoding("UTF-8");
-    String action = request.getParameter("a");
-
-    if ("joinform".equals(action)) {
-      request.getRequestDispatcher("/WEB-INF/views/user/joinform.jsp").forward(request, response);
-    } else if ("join".equals(action)) {
-      String name = request.getParameter("name");
-      String email = request.getParameter("email");
-      String password = request.getParameter("password");
-      String gender = request.getParameter("gender");
-
-      UserVo vo = new UserVo();
-      vo.setName(name);
-      vo.setEmail(email);
-      vo.setPassword(password);
-      vo.setGender(gender);
-
-      System.out.println(vo);
-      new UserDao().insert(vo);
-
-      // 브라우저에게 아래 joinsuccess 절대 경로로 리다이렉트
-      // response.sendRedirect(request.getContextPath()+"/user?a=joinsuccess");
-      response.sendRedirect(request.getContextPath() + "/user?a=joinsuccess");
-
-    } else if ("joinsuccess".equals(action)) {
-      request.getRequestDispatcher("/WEB-INF/views/user/joinsuccess.jsp").forward(request,
-          response);
-    } else {
-      response.sendRedirect(request.getContextPath());
-
-    }
+  @Override
+  protected Action getAction(String actionName) {
+    //return mapAction.get(actionName);
+    //else문 처리, key 없는 경우 default 처리 
+    return mapAction.getOrDefault(actionName, new MainAction());
   }
 
-  protected void doPost(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
-    // TODO Auto-generated method stub
-    doGet(request, response);
-  }
+  // protected void doGet(HttpServletRequest request, HttpServletResponse response)
+  // throws ServletException, IOException {
+  // request.setCharacterEncoding("UTF-8");
+  // String action = request.getParameter("a");
+  //
+  // if ("joinform".equals(action)) {
+  // request.getRequestDispatcher("/WEB-INF/views/user/joinform.jsp").forward(request, response);
+  // } else if ("join".equals(action)) {
+  // String name = request.getParameter("name");
+  // String email = request.getParameter("email");
+  // String password = request.getParameter("password");
+  // String gender = request.getParameter("gender");
+  //
+  // UserVo vo = new UserVo();
+  // vo.setName(name);
+  // vo.setEmail(email);
+  // vo.setPassword(password);
+  // vo.setGender(gender);
+  //
+  // System.out.println(vo);
+  // new UserDao().insert(vo);
+  //
+  // // 브라우저에게 아래 joinsuccess 절대 경로로 리다이렉트
+  // // response.sendRedirect(request.getContextPath()+"/user?a=joinsuccess");
+  // response.sendRedirect(request.getContextPath() + "/user?a=joinsuccess");
+  //
+  // } else if ("joinsuccess".equals(action)) {
+  // request.getRequestDispatcher("/WEB-INF/views/user/joinsuccess.jsp").forward(request,
+  // response);
+  // } else {
+  // response.sendRedirect(request.getContextPath());
+  //
+  // }
+  // }
+  //
+  // protected void doPost(HttpServletRequest request, HttpServletResponse response)
+  // throws ServletException, IOException {
+  // // TODO Auto-generated method stub
+  // doGet(request, response);
+  // }
 
 }
---
+++ mysite2/src/main/java/com/poscodx/mysite/controller/action/user/LoginAction.java
@@ -0,0 +1,33 @@
+package com.poscodx.mysite.controller.action.user;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.poscodx.mysite.controller.ActionServlet.Action;
+import com.poscodx.mysite.dao.UserDao;
+import com.poscodx.mysite.vo.UserVo;
+
+public class LoginAction implements Action {
+
+  @Override
+  public void execute(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    String email = request.getParameter("email");
+    String password = request.getParameter("password");
+
+    UserVo authUser = new UserDao().findByNoAndPassword(email, password);
+
+    // login 실패
+    if (authUser == null) {
+      // email, password가 틀린 경우 -> 무엇이 틀렸는지 알려주진 않음 왜냐? 보안상 문제
+      // forwarding or redirect로 다시 loginform으로 보내주기
+      request.setAttribute("email", email);
+      request.setAttribute("result", "fail");
+      request.getRequestDispatcher("/WEB-INF/views/user/loginform.jsp").forward(request, response);
+      return;
+    }
+    // login 처리
+  }
+
+}
