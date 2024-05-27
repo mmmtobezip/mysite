eb4ba729e98b17bddfc6e5f60366b7db891201e5
mmmtobezip <mmmszip@gmail.com>
2024-05-27 14:59:46 +0900

    feat: BoardServlet 생성

---
+++ mysite2/src/main/java/com/poscodx/mysite/controller/BoardServlet.java
@@ -0,0 +1,13 @@
+package com.poscodx.mysite.controller;
+
+public class BoardServlet extends ActionServlet {
+  private static final long serialVersionUID = 1L;
+
+  @Override
+  protected Action getAction(String actionName) {
+    return null;
+  }
+  // 현재코드에선 액션이 없으므로 무조건 ActionServlet에 action = null이 들어가고 400 Error 할당
+  // localhost:8080/mysite2/board
+
+}
