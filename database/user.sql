67c5b1975c1e5f6f649d8a6c156b3809c3c52637
mmmtobezip <mmmszip@gmail.com>
2024-05-28 15:37:11 +0900

    add: login query

--- database/user.sql
+++ database/user.sql
@@ -3,9 +3,11 @@ desc user;
 -- join
 -- insert into user values(name, email, password, gender, join_date);
 -- join_date를 Date 데이터로 지정해서 now()를 하면 시간까지 나오므로 에러 발생 ->  current_date() 사용
-insert into user values(null, '관리자', 'admin@mysite.com', password('1234'), 'female', current_date());
+-- insert into user values(null, '관리자', 'admin@mysite.com', password('1234'), 'female', current_date());
 
 -- test
 select * from user;
-
 select * from guestbook;
+
+-- user login(in mysite2/UserDao)
+select no, name from user where email = 'robin@gmail.com' and password=password('robin');
