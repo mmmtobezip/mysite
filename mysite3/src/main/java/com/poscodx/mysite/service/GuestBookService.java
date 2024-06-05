218860149418652d1ed6acf30b0610c462c7dae0
mmmtobezip <mmmszip@gmail.com>
2024-06-05 16:15:21 +0900

    refactor: mybatis 추가 및 Guestbook Sql session 구현

--- mysite3/src/main/java/com/poscodx/mysite/service/GuestBookService.java
+++ mysite3/src/main/java/com/poscodx/mysite/service/GuestBookService.java
@@ -1,23 +1,32 @@
 package com.poscodx.mysite.service;
 
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.poscodx.mysite.repository.GuestBookRepository;
 import com.poscodx.mysite.vo.GuestBookVo;
 
 @Service
 public class GuestBookService {
 
+  @Autowired
+  private GuestBookRepository guestbookRepository;
+
   // 전체 게시글 가져오기
   public List<GuestBookVo> getContentsList() {
-    return null;
+    return guestbookRepository.findAll();
   }
 
   // 게시글 지우기 - no & password 이용
-  public void deleteContents(Long no, String password) {}
+  public void deleteContents(Long no, String password) {
+    guestbookRepository.deleteByNoAndPassword(no, password);
+  }
 
   // 게시글 추가
   public void addContents(GuestBookVo vo) {
-
+    System.out.println(vo);
+    guestbookRepository.insert(vo);
+    System.out.println(vo);
   }
-
 }
+
