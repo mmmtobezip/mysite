f865bdb72a1afcc9009d2120e40f6a08c30df35b
mmmtobezip <mmmszip@gmail.com>
2024-06-07 15:56:22 +0900

    feat: @Transacational을 통한 Transaction

--- mysite3/src/main/java/com/poscodx/mysite/service/GuestBookService.java
+++ mysite3/src/main/java/com/poscodx/mysite/service/GuestBookService.java
@@ -3,30 +3,39 @@ package com.poscodx.mysite.service;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.poscodx.mysite.repository.GuestBookLogRepository;
 import com.poscodx.mysite.repository.GuestBookRepository;
 import com.poscodx.mysite.vo.GuestBookVo;
 
 @Service
 public class GuestBookService {
-
   @Autowired
-  private GuestBookRepository guestbookRepository;
+  private GuestBookRepository guestBookRepository;
+  @Autowired
+  private GuestBookLogRepository guestBookLogRepository;
 
   // 전체 게시글 가져오기
   public List<GuestBookVo> getContentsList() {
-    return guestbookRepository.findAll();
+    return guestBookRepository.findAll();
   }
 
   // 게시글 지우기 - no & password 이용
+  @Transactional
   public void deleteContents(Long no, String password) {
-    guestbookRepository.deleteByNoAndPassword(no, password);
+    guestBookLogRepository.update(no);
+    guestBookRepository.deleteByNoAndPassword(no, password);
   }
 
   // 게시글 추가
+  @Transactional
   public void addContents(GuestBookVo vo) {
-    System.out.println(vo);
-    guestbookRepository.insert(vo);
-    System.out.println(vo);
+    int count = guestBookLogRepository.update();
+
+    if (count == 0) {
+      guestBookLogRepository.insert();
+    }
+    guestBookRepository.insert(vo);
   }
 }
 
