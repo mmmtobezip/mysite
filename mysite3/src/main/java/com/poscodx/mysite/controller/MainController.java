80d5ae3651e3a7b51eefbbb8af6f6fe0d54cef78
mmmtobezip <mmmszip@gmail.com>
2024-06-12 14:36:05 +0900

    feat: logging 적용

--- mysite3/src/main/java/com/poscodx/mysite/controller/MainController.java
+++ mysite3/src/main/java/com/poscodx/mysite/controller/MainController.java
@@ -4,7 +4,9 @@ import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import com.poscodx.mysite.service.SiteService;
+import com.poscodx.mysite.vo.UserVo;
 
 @Controller
 public class MainController {
@@ -18,5 +20,28 @@ public class MainController {
   public String index(HttpServletRequest request) {
     ServletContext sc = request.getServletContext();
     return "main/index";
+  }
+
+  @ResponseBody
+  @RequestMapping("/msg01")
+  public String message01() {
+    return "Hello World";
+  }
+
+  @ResponseBody
+  @RequestMapping("/msg02")
+  public String message02(String name) {
+    return "안녕~ " + name;
+  }
+
+  @ResponseBody
+  @RequestMapping("/msg03")
+  public Object message03() {
+    UserVo vo = new UserVo();
+    vo.setNo(1L);
+    vo.setName("둘리");
+    vo.setEmail("dooly@gmail.com");
+
+    return vo;
   }
 }
--- mysite3/src/main/java/com/poscodx/mysite/exception/GlobalExceptionHandler.java
+++ mysite3/src/main/java/com/poscodx/mysite/exception/GlobalExceptionHandler.java
@@ -2,25 +2,30 @@ package com.poscodx.mysite.exception;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+  private static final Log logger = LogFactory.getLog(GlobalExceptionHandler.class);
 
   @ExceptionHandler(Exception.class) // 받아야할 exception 종류를 적어줌. controller에서 발생하는 exception의 부모를
-                                     // 명시해주면,
+                                     // 명시하는데
   // checkedexception도 throws로 위로 toss할 수 있는데, 모든 excepton을 받기 위해 부모의 exception.class를 명시해줌.
   // 만약 종류별로 처리하고 싶으면 SQL Exception처리 전용 메서드 등을 따로 분리해줘도
   public String handler(Exception e, Model model) {
     // 1. 로깅(logging)
-    StringWriter error = new StringWriter(); // StringWrite안에 buffer 메모리에 wirte
-    e.printStackTrace(new PrintWriter(error));
-    System.out.println(error.toString());
+    StringWriter errors = new StringWriter(); // StringWrite안에 buffer 메모리에 wirte
+    e.printStackTrace(new PrintWriter(errors));
+
+    // System.out.println(error.toString()); -> logback.xml에서 처리
+    logger.error(errors.toString());
 
     // 2. 사과(종료)
-    model.addAttribute("error", error.toString());
+    model.addAttribute("error", errors.toString());
     return "errors/exception";
   }
 }
