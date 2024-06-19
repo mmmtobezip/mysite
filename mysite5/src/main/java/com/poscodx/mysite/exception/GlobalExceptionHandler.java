0c49b6f5d11c5efab1f7ef2da7b013144878d609
mmmtobezip <mmmszip@gmail.com>
2024-06-19 15:09:13 +0900

    feat: 404 error 처리

--- mysite5/src/main/java/com/poscodx/mysite/exception/GlobalExceptionHandler.java
+++ mysite5/src/main/java/com/poscodx/mysite/exception/GlobalExceptionHandler.java
@@ -11,6 +11,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.poscodx.mysite.dto.JsonResult;
 
@@ -44,9 +45,13 @@ public class GlobalExceptionHandler {
       os.close();
     } else {
       // 4. 사과 페이지(정상 종료)
-      request.setAttribute("error", errors.toString());
-      request.getRequestDispatcher("/WEB-INF/views/errors/exception.jsp").forward(request,
-          response);
+      if (e instanceof NoHandlerFoundException) {
+        request.getRequestDispatcher("/WEB-INF/views/errors/404.jsp").forward(request, response);
+      } else {
+        request.setAttribute("error", errors.toString());
+        request.getRequestDispatcher("/WEB-INF/views/errors/exception.jsp").forward(request,
+            response);
+      }
     }
   }
 }
